3f6fbc02497860281a732ce390029f5a55a5ded8
QDD0 <dend51108@gmail.com>
2026-02-07 18:05:38 +0300

    testing repo 2

---
+++ src/main/java/org/example/mysecurity/models/Person.java
@@ -0,0 +1,13 @@
+package org.example.mysecurity.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "person")
+@Data
+public class Person {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id_person;
+}
